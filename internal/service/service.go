9779b79e0b1df4b8187af29a9f2061e71ca451f0
AeZG <gregorydyuldin@gmail.com>
2024-09-02 16:00:45 +0300

    Add service layer methods

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,32 +1,43 @@
 package service
 
+import (
+	"ChaikaReports/internal/models"
+	"ChaikaReports/internal/repository"
+)
+
 type SalesService interface {
-	InsertData()
-	GetEmployeeCartsInTrip()
-	GetEmployeeIDsByTrip()
-	UpdateItemQuantity()
-	DeleteItemFromCart()
+	InsertData(carriageReport *models.Carriage) error
+	GetEmployeeCartsInTrip(tripID *models.TripID, employeeID *string) ([]models.Cart, error)
+	GetEmployeeIDsByTrip(tripID *models.TripID) ([]string, error)
+	UpdateItemQuantity(tripID *models.TripID, cartID *models.CartID, productID *int, newQuantity *int16) error
+	DeleteItemFromCart(tripID *models.TripID, cartID *models.CartID, productID *int) error
 }
 
 type salesService struct {
+	repo repository.SalesRepository
 }
 
-func NewSalesService() SalesService {
-
+// Creates new salesService
+func NewSalesService(repo repository.SalesRepository) SalesService {
+	return &salesService{repo: repo}
 }
 
-func (s *salesService) InsertData() error {
+func (s *salesService) InsertData(carriageReport *models.Carriage) error {
+	return s.repo.InsertData(carriageReport)
 }
 
-func (s *salesService) GetEmployeeCartsInTrip() {
+func (s *salesService) GetEmployeeCartsInTrip(tripID *models.TripID, employeeID *string) ([]models.Cart, error) {
+	return s.repo.GetEmployeeCartsInTrip(tripID, employeeID)
 }
 
-func (s *salesService) GetEmployeeIDsByTrip() {
+func (s *salesService) GetEmployeeIDsByTrip(tripID *models.TripID) ([]string, error) {
+	return s.GetEmployeeIDsByTrip(tripID)
 }
 
-func (s *salesService) UpdateItemQuantity() {
+func (s *salesService) UpdateItemQuantity(tripID *models.TripID, cartID *models.CartID, productID *int, newQuantity *int16) error {
+	return s.UpdateItemQuantity(tripID, cartID, productID, newQuantity)
 }
 
-func (s *salesService) DeleteItemFromCart() {
-
+func (s *salesService) DeleteItemFromCart(tripID *models.TripID, cartID *models.CartID, productID *int) error {
+	return s.DeleteItemFromCart(tripID, cartID, productID)
 }
